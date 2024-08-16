f63054b47f1a2f024000f6844f518f2221f6d09d
Meme Guy <owen.debiasio@gmail.com>
2024-08-16 17:17:34 -0400

    Add files via upload

---
+++ dtwu/installeriguess.cmd
@@ -0,0 +1,3 @@
+winget install python.python.3.12
+main.py
+exit
---
+++ dtwu/psscripts/WinUtilInstaller.cmd
@@ -0,0 +1,1 @@
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -Command "Start-Process powershell.exe -verb runas -ArgumentList 'irm https://christitus.com/win | iex'
